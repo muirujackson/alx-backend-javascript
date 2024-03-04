44d6d14c63c4b46003169b3469aa060925e55a6b
muirujackson <jakezweru@gmail.com>
2024-03-04 18:54:26 +0000

    quiz 9 redo

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -11,21 +11,5 @@ export default function getFullBudgetObject(income, gdp, capita) {
       return `${income} euros`;
     },
   };
-
-  return fullBudget;
-import getBudgetObject from './7-getBudgetObject';
-
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
-
   return fullBudget;
 }
